34e663988c8bea838fc463be4cb3544e89321973
Athul <s3958556@student.rmit.edu.au>
2025-10-12 17:58:42 +1100

    feat: implemented barebones tracker page

---
+++ frontend/src/pages/tracker.tsx
@@ -0,0 +1,62 @@
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Head from "next/head";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { DashboardNavigation } from "@/components/DashboardNavigation"; 
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner"
+
+export default function TrackerPage() {
+    const { isAuthenticated, authLoading } = useAuth();
+    const router = useRouter();
+
+    // Redirect to login if not authenticated
+    useEffect(() => {
+        if (!authLoading && !isAuthenticated) {
+            router.push("/login");
+        }
+    }, [isAuthenticated, authLoading, router]);
+    
+    // Show loading state when logging out
+    if (authLoading || !isAuthenticated) {
+        return (
+            <div className="flex justify-center items-center h-screen bg-background text-foreground">
+                <Spinner className="size-15"/>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Application Tracker | JobLog</title>
+            </Head>
+            <Header />
+            
+            <main className="container mx-auto p-4 sm:p-8 min-h-[calc(100vh-128px)]">
+                <DashboardNavigation currentPath={router.pathname} />
+
+                <div className="space-y-8">
+                    <div className="flex justify-between items-center">
+                        <h1 className="text-3xl font-extrabold tracking-tight">
+                            Application Tracker
+                        </h1>
+                        <Button className="text-base">
+                            + Add New Application
+                        </Button>
+                    </div>
+
+                    {/* Placeholder for Table */}
+                    <div className="p-8 border border-border rounded-lg min-h-[300px] flex items-center justify-center ring-2 ring-primary/40 dark:bg-card">
+                        <p className="text-muted-foreground">Table goes here</p>
+                    </div>
+                </div>
+            </main>
+
+            <Footer />
+        </>
+    );
+}

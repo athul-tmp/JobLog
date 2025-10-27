25463584acc5b485e3b4d43665cb84047997e0ce
Athul <s3958556@student.rmit.edu.au>
2025-10-28 01:38:49 +1100

    updated favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="dark">
-      <Head />
+      <Head><link rel="icon" type="image/svg+xml" href="/favicon.svg" /></Head>
       <body className="antialiased">
         <Main />
         <NextScript />

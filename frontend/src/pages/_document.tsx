70898b127c12134b65769ffac4b3c33bd628d967
Athul <s3958556@student.rmit.edu.au>
2025-11-01 00:01:37 +1100

    fixed favicon light/dark switching

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="dark">
-      <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon_light.svg" media="(prefers-color-scheme: light)" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" media="(prefers-color-scheme: dark)" />
-      </Head>
+      <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
